6ee3a103f01d2d3ab4e489606aa9dfb28c7d9c20
Noor Fatma <nfatma8989@gmail.com>
2024-01-08 21:07:23 +0530

    Checking First Name is valid or not

--- RegExExample.java
+++ RegExExample.java
@@ -1,9 +1,20 @@
 package com.bridgeLabz.regularExpression;
 
+import java.util.regex.*;
+
 public class RegExExample {
 	
 	public static void main(String args[]) {
-		System.out.println("Welcome to User Registration System");
+		
+		String regularExp="^[A-Z][A-Za-z]{2,}$";
+		String firstName="Noor";
+		Pattern pattern=Pattern.compile(regularExp);
+		Matcher matcher=pattern.matcher(firstName);
+		if (matcher.matches()==true)
+			System.out.println("Valid First Name");
+		else
+			System.out.println("Invalid First Name");
+		
 	}
 	
 }
